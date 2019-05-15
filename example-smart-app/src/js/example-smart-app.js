825c1b2cb2c13f09c3e9450b9445cfd27d669cf1
Jeno Pizarro <jjpizarr@us.ibm.com>
2019-05-15 16:42:51 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -23,7 +23,7 @@
                   });
 
         $.when(pt, obv).fail(onError);
-        console.log(JSON.stringify(obv))
+        console.log(typeof obv)
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
